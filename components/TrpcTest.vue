851fd1cc33ae8f54f5196d972c115e8e1637b3d3
xhayper <46065349+xhayper@users.noreply.github.com>
2024-07-24 10:18:43 +0700

    feat: more progress

--- components/TrpcTest.vue
+++ components/TrpcTest.vue
@@ -2,10 +2,12 @@
 const { $client } = useNuxtApp()
 
 const userData = await $client.me.useQuery();
+const myGrade = $client.get_grades.useQuery();
 </script>
 
 <template>
   <div>
     <p>{{ JSON.stringify(userData.data?.value) }}</p>
+    <p>{{ JSON.stringify(myGrade.data?.value) }}</p>
   </div>
 </template>
--- server/trpc/routers/index.ts
+++ server/trpc/routers/index.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod'
 import { publicProcedure, authedProcedure, router } from '../trpc'
+import { TRPCError } from '@trpc/server'
 
 export const appRouter = router({
     // hello: publicProcedure
@@ -17,8 +18,20 @@ export const appRouter = router({
     me: authedProcedure
         .query(async ({ ctx }) => {
             return (await ctx.supabaseServiceRole.from('user_data').select('*').eq('id', ctx.supabaseSession!.user.id).single()).data!
-        })
+        }),
 
+    get_grades: authedProcedure
+        .input(
+            z.object({
+                id: z.string().optional(),
+            }).optional()
+        )
+        .query(async ({ ctx, input }) => {
+            if (input && ctx.user!.role === "STUDENT" && ctx.user!.id !== input?.id)
+                throw new Error("You are not allowed to view other students' grades")
+
+            return (await ctx.supabaseServiceRole.from('grade').select('*').eq('user_id', input?.id ?? ctx.user!.id)).data;
+        })
 })
 
 // export type definition of API
