400536042617ec8b4909a101792997d973b6e4c8
xhayper <hayper1919@gmail.com>
2024-07-24 10:18:43 +0700

    feat: more progress

--- server/trpc/context.ts
+++ server/trpc/context.ts
@@ -6,10 +6,23 @@ import type { H3Event } from "h3";
  * Creates context for an incoming request
  * @link https://trpc.io/docs/context
  */
-export const createContext = async (event: H3Event) => ({
-    supabaseServiceRole: serverSupabaseServiceRole(event),
-    supabaseSession: await serverSupabaseSession(event),
-    supabaseClient: await serverSupabaseClient(event)
-})
+export const createContext = async (event: H3Event) => {
+    const supabaseConstant = {
+        supabaseServiceRole: serverSupabaseServiceRole(event),
+        supabaseSession: await serverSupabaseSession(event),
+        supabaseClient: await serverSupabaseClient(event),
+    }
+
+    const userData: any =
+        supabaseConstant.supabaseSession ?
+            (await supabaseConstant.supabaseServiceRole.from('user_data').select('*').eq('id', supabaseConstant.supabaseSession.user.id).single()).data! : null;
+
+    return {
+        ...supabaseConstant,
+
+        // User data
+        user: userData
+    }
+};
 
 export type Context = inferAsyncReturnType<typeof createContext>;
