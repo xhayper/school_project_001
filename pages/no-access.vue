d7ff3ef01790e32ef301e01e8466990f8901765c
xhayper <46065349+xhayper@users.noreply.github.com>
2024-07-31 22:22:35 +0700

    fix: more bugs

--- pages/no-access.vue
+++ pages/no-access.vue
@@ -12,6 +12,7 @@
       <TriangleAlertIcon class="mx-auto h-12 w-12 text-primary" />
       <h1 class="mt-4 text-3xl font-bold tracking-tight text-foreground sm:text-4xl">Access Denied</h1>
       <p class="mt-4 text-muted-foreground">You do not have permission to access this page.</p>
+      <Button variant="outline" type="submit" class="w-full my-5" @click="navigateTo('/')"> Go back </Button>
     </div>
   </div>
 </template>
