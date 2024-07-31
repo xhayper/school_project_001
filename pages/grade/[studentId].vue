a7ebf6f7f6ac1cd2c05b3fb3e18190749be2991d
xhayper <46065349+xhayper@users.noreply.github.com>
2024-07-31 09:06:54 +0700

    feat: more features

--- pages/grade/[studentId].vue
+++ pages/grade/[studentId].vue
@@ -13,6 +13,124 @@ const parsedNumber = parseInt(route.params.studentId as string);
 const gradeRef = ref<Database['public']['Tables']['grade']['Row'][] | null>(null);
 const studentInfoRef = ref<Database['public']['Tables']['user_data']['Row'] | null>(null);
 
+const toPascalCase = (str: string) => str.replace(/(\w)(\w*)/g, (_, g1, g2) => g1.toUpperCase() + g2.toLowerCase());
+
+const gradeList: {
+  [key in Database['public']['Enums']['grade_subject']]: number;
+} = {
+  MATH: 0,
+  BIOLOGY: 0,
+  ENGLISH: 0,
+  PHYSICS: 0,
+  HISTORY: 0,
+  ECONOMICS: 0,
+  CHEMISTRY: 100
+};
+
+type HOLLAND_CODE = 'Realistic' | 'Investigative' | 'Artistic' | 'Social' | 'Enterprising' | 'Conventional';
+
+const averageGPAPerHollandCode: { [key in HOLLAND_CODE]: { minGPA: number; maxGPA: number } } = {
+  Realistic: { minGPA: 3.0, maxGPA: 3.5 },
+  Investigative: { minGPA: 3.2, maxGPA: 3.7 },
+  Artistic: { minGPA: 3.0, maxGPA: 3.5 },
+  Social: { minGPA: 3.1, maxGPA: 3.6 },
+  Enterprising: { minGPA: 3.0, maxGPA: 3.5 },
+  Conventional: { minGPA: 3.1, maxGPA: 3.6 }
+};
+
+const jobList: {
+  [key in HOLLAND_CODE]: {
+    name: string;
+    scores: { subject: Database['public']['Enums']['grade_subject']; min: number }[];
+  }[];
+} = {
+  Realistic: [
+    { name: 'Math', scores: [{ subject: 'MATH', min: 75 }] },
+    {
+      name: 'Science',
+      scores: [
+        { subject: 'PHYSICS', min: 75 },
+        { subject: 'BIOLOGY', min: 75 },
+        { subject: 'PHYSICS', min: 75 },
+        { subject: 'CHEMISTRY', min: 75 }
+      ]
+    },
+    { name: 'Technology', scores: [{ subject: 'MATH', min: 75 }] },
+    {
+      name: 'Engineering',
+      scores: [
+        { subject: 'MATH', min: 75 },
+        { subject: 'PHYSICS', min: 75 }
+      ]
+    }
+  ],
+  Investigative: [
+    { name: 'Math', scores: [{ subject: 'MATH', min: 75 }] },
+    {
+      name: 'Science',
+      scores: [
+        { subject: 'PHYSICS', min: 75 },
+        { subject: 'BIOLOGY', min: 75 },
+        { subject: 'PHYSICS', min: 75 },
+        { subject: 'CHEMISTRY', min: 75 }
+      ]
+    },
+    {
+      name: 'Computer Science',
+      scores: [
+        { subject: 'MATH', min: 75 },
+        { subject: 'PHYSICS', min: 75 }
+      ]
+    }
+  ],
+  // empty for now
+  Artistic: [],
+  Social: [],
+  Enterprising: [],
+  Conventional: []
+};
+
+const GPA = computed(() => {
+  const totalGrade = Object.values(gradeList).reduce((acc, grade) => acc + grade, 0);
+  return (totalGrade / (100 * Object.keys(gradeList).length)) * 4;
+});
+
+const SubjectGPA = computed(() => {
+  return Object.entries(gradeList).reduce(
+    (acc, [subject, grade]) => {
+      (acc as any)[subject] = (grade / 100) * 4;
+      return acc;
+    },
+    {} as { [key in Database['public']['Enums']['grade_subject']]: number }
+  );
+});
+
+const averageHollandCode = computed(() => {
+  const gpa = GPA.value;
+  return Object.entries(averageGPAPerHollandCode)
+    .filter(([_, { minGPA, maxGPA }]) => gpa >= minGPA && gpa <= maxGPA)
+    .map(([name, { minGPA, maxGPA }]) => ({ name, minGPA, maxGPA }));
+});
+
+const availableJobs = computed(() => {
+  return Object.entries(jobList).reduce(
+    (acc, [hollandCode, jobs]) => {
+      const availableJobs = jobs.filter((job) => {
+        return job.scores.every((score) => {
+          return gradeList[score.subject] >= score.min;
+        });
+      });
+
+      if (availableJobs.length > 0) {
+        (acc as any)[hollandCode] = availableJobs.map((job) => job.name);
+      }
+
+      return acc;
+    },
+    {} as { [key in HOLLAND_CODE]: string[] }
+  );
+});
+
 if (!Number.isNaN(parsedNumber)) {
   const isStudentExists = await $client.user.exists.query(parsedNumber);
   userExists.value = isStudentExists != null && isStudentExists;
@@ -33,7 +151,13 @@ if (userExists) {
       })
     ).value as any;
 
-    console.log(gradeRef.value);
+    if (gradeRef.value == null) {
+      gradeRef.value = [];
+    }
+
+    gradeRef.value.forEach((grade) => {
+      gradeList[grade.subject] = grade.grade;
+    });
   }
 }
 </script>
@@ -49,6 +173,37 @@ if (userExists) {
       </div>
 
       <div>Student name: {{ studentInfoRef!.name }}</div>
+      <br />
+      <div>Overall GPA: {{ Math.round(GPA * 100) / 100 }}</div>
+      <br />
+      <div>Subject GPA:</div>
+      <ul>
+        <li v-for="[subject, gpa] in Object.entries(SubjectGPA)" :key="subject">
+          {{ toPascalCase(subject) }}: {{ Math.round(gpa * 100) / 100 }}
+        </li>
+      </ul>
+      <br />
+
+      <div v-if="averageHollandCode.length > 0">
+        <div>You are in the average range for these Hollan Code</div>
+        <br />
+        <ul>
+          <li v-for="hollandCode in averageHollandCode" :key="hollandCode.name">
+            <div>{{ hollandCode.name }}</div>
+            <div>GPA: {{ hollandCode.minGPA }} - {{ hollandCode.maxGPA }}</div>
+            <div v-if="(availableJobs as any)[hollandCode.name] && (availableJobs as any)[hollandCode.name].length > 0">
+              <br />
+              <div>Job that fits you in these categoires:</div>
+              <ul>
+                <li v-for="job in (availableJobs as any)[hollandCode.name]" :key="job">
+                  {{ job }}
+                </li>
+              </ul>
+            </div>
+            <br />
+          </li>
+        </ul>
+      </div>
     </div>
     <div>
       <NuxtLink to="/">Back</NuxtLink>
