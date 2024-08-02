41403ee66122b576c23db9fe86a50a2b4919e2f3
xhayper <hayper1919@gmail.com>
2024-08-02 18:53:43 +0700

    feat: add analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,9 @@ export default defineNuxtConfig({
     }
   },
   supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_ANON_KEY,
+    serviceKey: process.env.SUPABASE_SERVICE_KEY,
     redirectOptions: {
       login: '/login',
       callback: '/confirm'
