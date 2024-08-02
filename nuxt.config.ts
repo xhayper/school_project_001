a8875da7a1e1ceffd5d2409e242d5de9c3c157fe
xhayper <hayper1919@gmail.com>
2024-08-02 18:54:34 +0700

    fix: wrong env name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_ANON_KEY,
-    serviceKey: process.env.SUPABASE_SERVICE_KEY,
+    serviceKey: process.env.SUPABASE_SERVICE_ROLE_KEY,
     redirectOptions: {
       login: '/login',
       callback: '/confirm'
